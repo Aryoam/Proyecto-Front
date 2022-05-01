4465d3cd863ade8d4764104c5600077b86bc37c1
Aryoam Navas <aryoam96@gmail.com>
2022-05-01 20:07:00 +0200

    Graficos funcionando v1

--- src/components/Grafico.jsx
+++ src/components/Grafico.jsx
@@ -1,22 +1,92 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { render } from "react-dom";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-
-const options = {
-  title: {
-    text: "Grafico del paciente",
-  },
-  series: [
-    {
-      data: [1, 2, 3],
-    },
-  ],
-};
+import SelectGraficos from "./SelectGraficos";
 
 const Grafico = ({ data }) => {
-  console.log(data);
-  return <HighchartsReact highcharts={Highcharts} options={options} />;
+  const [valores, setValores] = useState(null);
+  const [fecha, setFecha] = useState(null);
+  const [busqueda, setBusqueda] = useState({
+    tipo: "Temperatura",
+    db: "temperatura",
+  });
+  const options = {
+    title: {
+      text: `Historico de ${data.nombre}`,
+    },
+
+    subtitle: {
+      text: "Temperatura",
+    },
+    series: [
+      {
+        name: "Día",
+        data: valores,
+      },
+    ],
+    xAxis: {
+      categories: fecha,
+    },
+    yAxis: {
+      title: {
+        text: "Temperatura (°C)",
+      },
+      min: 35,
+      max: 40,
+    },
+    plotOptions: {
+      line: {
+        dataLabels: {
+          enabled: true,
+        },
+        enableMouseTracking: false,
+      },
+    },
+  };
+
+  useEffect(() => {
+    try {
+      const consultarApi = async () => {
+        const url = `http://localhost:4000/api/paciente/buscar-historial/${data._id}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        const array = resultado.historial;
+        const consulta = busqueda.db;
+
+        const datos = array.map((datos) => {
+          if (consulta == "temperatura") {
+            return datos.temperatura;
+          } else if (consulta == "cardiaca") {
+            return datos.cardiaca;
+          } else if (consulta == "respiratoria") {
+            return datos.respiratoria;
+          } else if (consulta == "oxigeno") {
+            return datos.oxigeno;
+          } else if (consulta == "arterial") {
+            return datos.arterial;
+          } else if (consulta == "actitud") {
+            return datos.actitud;
+          }
+        });
+        const fechas = array.map((fecha) => fecha.fecha);
+        setValores(datos);
+        setFecha(fechas);
+      };
+      consultarApi();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [busqueda]);
+
+  return (
+    <>
+      <SelectGraficos selected={busqueda} setSelected={setBusqueda} />
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </>
+  );
 };
 
 export default Grafico;
